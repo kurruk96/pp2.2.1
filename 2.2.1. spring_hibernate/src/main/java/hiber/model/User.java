e5ef9c6b73641c6c7f81adb5dbb9a07c41c0121b
Noteburg <kurruk72@gmail.com>
2023-04-01 13:04:10 +0300

    Исправил ошибку

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -6,86 +6,81 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   @Column(name = "name")
-   private String firstName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
-   @Column(name = "email")
-   private String email;
+    @Column(name = "email")
+    private String email;
 
-   @OneToOne
-   @JoinColumn(name = "cars_id")
-   private Car userCar;
+    @OneToOne
+    @JoinColumn(name = "cars_id")
+    private Car userCar;
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    public User() {
+    }
 
-   public User(String firstName, String lastName, String email, Car userCar) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-      this.userCar = userCar;
-   }
+    public User(String firstName, String lastName, String email, Car userCar) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.userCar = userCar;
+    }
 
-   public Car getUserCar() {
-      return userCar;
-   }
+    public Car getUserCar() {
+        return userCar;
+    }
 
-   public void setUserCar(Car userCar) {
-      this.userCar = userCar;
-   }
+    public void setUserCar(Car userCar) {
+        this.userCar = userCar;
+    }
 
-   public Long getId() {
-      return id;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public String getEmail() {
-      return email;
-   }
+    public String getEmail() {
+        return email;
+    }
 
-   @Override
-   public String toString() {
-      return "User{" +
-              "id=" + id +
-              ", firstName='" + firstName + '\'' +
-              ", lastName='" + lastName + '\'' +
-              ", email='" + email + '\'' +
-              ", userCar=" + userCar +
-              '}';
-   }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", userCar=" + userCar +
+                '}';
+    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
@@ -2,7 +2,6 @@ package hiber.service;
 
 import hiber.dao.CarDao;
 import hiber.model.Car;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,9 +9,11 @@ import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService{
-
-    @Autowired
     private CarDao carDao;
+
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
 
     @Transactional
     @Override
